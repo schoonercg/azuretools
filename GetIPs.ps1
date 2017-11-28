e4aeaa9667adb25cb381413fd21a1a8b91eda414
Garza <garza@netrun.net>
2017-11-28 09:06:37 -0800

    Add files via upload

---
+++ GetIPs.ps1
@@ -0,0 +1,17 @@
+﻿##Azure Script to Obtain outbound IP addresses for each webapp in a named Resource Group
+ [CmdletBinding()]
+Param( 
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String] 
+        $Name 
+        )
+ $scSites = Get-AzureRmWebApp -ResourceGroupName $Name 
+    foreach ($site in $scSites)
+    {
+     write-host "Web App: $($site.DefaultHostName)" -ForegroundColor Green 
+   (Get-AzureRmResource -ResourceGroupName $Name -ResourceType Microsoft.Web/sites -ResourceName $site.SiteName).OutboundIpAddresses
+
+     write-host "web App: $($site.OutboundIpAddresses)" -ForegroundColor Yellow
+
+    }
