a7ed419f800a457d0e42e578cee27f487c9221f4
Garza <garza@netrun.net>
2021-08-27 09:42:27 -0700

    Create cef2.sh

---
+++ cef2.sh
@@ -0,0 +1,12 @@
+#wget https://aka.ms/dependencyagentlinux
+#sh InstallDependencyAgent-Linux64.bin -s
+
+##install and apply the CEF collector
+yum update
+wget -O prepare_vm_disks.sh https://raw.githubusercontent.com/Azure-Samples/compute-automation-configurations/master/prepare_vm_disks.sh
+prepare_vm_disks.sh
+wget -O cef_installer.py https://raw.githubusercontent.com/schoonercg/azuretools/master/cef_installer.py
+python3 cef_installer.py $1 $2
+##Changing the source of the TimeGenerated field
+wget -O TimeGeneratedG.py https://raw.githubusercontent.com/schoonercg/azuretools/master/Time_GeneratedG.py
+python3 TimeGeneratedG.py $1
