1d8ee20a4b56cfc8ace7cead9443137f13455fe1
Garza <garza@netrun.net>
2021-09-02 12:32:07 -0700

    Update cef.sh

--- cef.sh
+++ cef.sh
@@ -9,10 +9,10 @@ wget -O prepare_vm_disks.sh https://raw.githubusercontent.com/schoonercg/azureto
 sudo chmod +x ./prepare_vm_disks.sh
 sudo ./prepare_vm_disks.sh
 wget -O cef_installer.py https://raw.githubusercontent.com/schoonercg/azuretools/master/cef_installer.py
-python3 cef_installer.py $1 $2
+sudo python3 cef_installer.py $1 $2
 ##Changing the source of the TimeGenerated field
 wget -O TimeGeneratedG.py https://raw.githubusercontent.com/schoonercg/azuretools/master/Time_GeneratedG.py
-python3 TimeGeneratedG.py $1
+sudo python3 TimeGeneratedG.py $1
 sudo sed -i '46 s/^/#/' /etc/rsyslog.conf
 sudo sed -i '49 s/^/#/' /etc/rsyslog.conf
 sudo sed -i '52 s/^/#/' /etc/rsyslog.conf
