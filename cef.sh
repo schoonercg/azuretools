f8f6f6eddaffd716f1a8201a2744e42a644c4fdc
Garza <garza@netrun.net>
2021-09-02 13:26:21 -0700

    Update cef.sh

--- cef.sh
+++ cef.sh
@@ -1,4 +1,3 @@
-
 #wget https://aka.ms/dependencyagentlinux
 #sh InstallDependencyAgent-Linux64.bin -s
 
@@ -9,14 +8,11 @@ sudo sed -i 's/enforcing/permissive/g' /etc/selinux/config
 wget -O prepare_vm_disks.sh https://raw.githubusercontent.com/schoonercg/azuretools/master/prepare_vm_disks.sh
 sudo chmod +x ./prepare_vm_disks.sh
 sudo ./prepare_vm_disks.sh
-sleep 2m
 wget -O cef_installer.py https://raw.githubusercontent.com/schoonercg/azuretools/master/cef_installer.py
 sudo python3 cef_installer.py $1 $2
-sleep 2m
 ##Changing the source of the TimeGenerated field
 wget -O TimeGeneratedG.py https://raw.githubusercontent.com/schoonercg/azuretools/master/Time_GeneratedG.py
 sudo python3 TimeGeneratedG.py $1
-sleep 1m
 sudo sed -i '46 s/^/#/' /etc/rsyslog.conf
 sudo sed -i '49 s/^/#/' /etc/rsyslog.conf
 sudo sed -i '52 s/^/#/' /etc/rsyslog.conf
