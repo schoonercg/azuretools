6b4fed5c44bb309a80439f5facffa08a65f184e7
Garza <garza@netrun.net>
2022-07-18 21:05:29 -0700

    Add files via upload

--- cefpy.sh
+++ cefpy.sh
@@ -1,18 +1,5 @@
 #wget https://aka.ms/dependencyagentlinux
 #sh InstallDependencyAgent-Linux64.bin -s
-
-##install and apply the CEF collector
-apt-get update
-wget -O cef_installer.py https://remotestatessa.blob.core.windows.net/customlogs/cef_installer.py
-python3 cef_installer.py $1 $2
-##Changing the source of the TimeGenerated field
-wget -O TimeGeneratedG.py https://remotestatessa.blob.core.windows.net/customlogs/Time_GeneratedG.py
-python3 TimeGeneratedG.py $1
-
-
-#wget https://aka.ms/dependencyagentlinux
-#sh InstallDependencyAgent-Linux64.bin -s
-
 ##install and apply the CEF collector
 yum update -y
 yum install -y python2
@@ -20,10 +7,6 @@ sudo update-alternatives --remove-all python
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 1
 sudo setenforce 0
 sudo sed -i 's/enforcing/permissive/g' /etc/selinux/config
-#wget -O prepare_vm_disks.sh https://raw.githubusercontent.com/schoonercg/azuretools/master/prepare_vm_disks.sh
-#sudo chmod +x ./prepare_vm_disks.sh
-#sudo ./prepare_vm_disks.sh
-#sudo chmod -R 775 /opt
 sudo lvextend --size +28G /dev/mapper/rootvg-rootlv
 sudo xfs_growfs /dev/mapper/rootvg-rootlv
 wget -O cef_installer.py https://raw.githubusercontent.com/schoonercg/azuretools/master/cef_installer.py
