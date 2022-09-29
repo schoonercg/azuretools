231600aed230d94def3112bb1b3c25afb2c8f8b8
Garza <garza@netrun.net>
2022-09-29 13:39:24 -0700

    Update cefpy.sh

--- cefpy.sh
+++ cefpy.sh
@@ -22,4 +22,5 @@ sudo firewall-cmd --permanent --zone=public --add-port=514/tcp
 sudo firewall-cmd --permanent --zone=public --add-port=514/udp
 sudo firewall-cmd --reload
 sudo sleep 90s
-sudo echo "slept and output"
+sudo rm /var/lib/waagent/Microsoft.Azure.Diagnostics.LinuxDiagnostic-4.1.6/DistroSpecific.py
+sudo wget https://raw.githubusercontent.com/Azure/azure-linux-extensions/master/Diagnostic/DistroSpecific.py /var/lib/waagent/Microsoft.Azure.Diagnostics.LinuxDiagnostic-4.1.6/DistroSpecific.py
