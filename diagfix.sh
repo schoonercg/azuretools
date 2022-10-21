4e05e1df13edcd9dbcc4f4555756d3ffc1aa4bec
Garza <garza@netrun.net>
2022-10-21 11:56:10 -0700

    Update diagfix.sh

--- diagfix.sh
+++ diagfix.sh
@@ -1,3 +1,4 @@
 sudo sleep 60s
 sudo rm -f /var/lib/waagent/Microsoft.Azure.Diagnostics.LinuxDiagnostic-4.1.6/DistroSpecific.py
-sudo wget /var/lib/waagent/Microsoft.Azure.Diagnostics.LinuxDiagnostic-4.1.6/DistroSpecific.py https://raw.githubusercontent.com/Azure/azure-linux-extensions/master/Diagnostic/DistroSpecific.py -q
+sudo cd /var/lib/waagent/Microsoft.Azure.Diagnostics.LinuxDiagnostic-4.1.6/
+sudo wget https://raw.githubusercontent.com/Azure/azure-linux-extensions/master/Diagnostic/DistroSpecific.py -q
