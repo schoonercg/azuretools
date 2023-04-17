5427c8dfb76483d704f7b4036f0a431562c1dc01
Garza <garza@netrun.net>
2023-04-17 14:16:40 -0700

    Update cefpy.sh

--- cefpy.sh
+++ cefpy.sh
@@ -12,7 +12,9 @@ sudo xfs_growfs /dev/mapper/rootvg-rootlv
 wget -O cef_installer.py https://raw.githubusercontent.com/schoonercg/azuretools/master/cef_installer.py
 sudo python3 cef_installer.py $1 $2
 ##Changing the source of the TimeGenerated field
+wget -O Forwarder_AMA_installer.py https://raw.githubusercontent.com/schoonercg/azuretools/master/Forwarder_AMA_installer.py
 wget -O TimeGeneratedG.py https://raw.githubusercontent.com/schoonercg/azuretools/master/Time_GeneratedG.py
+sudo python3 Forwarder_AMA_installer.py
 sudo python3 TimeGeneratedG.py $1
 sudo sed -i '46 s/^/#/' /etc/rsyslog.conf
 sudo sed -i '49 s/^/#/' /etc/rsyslog.conf
@@ -23,6 +25,6 @@ sudo firewall-cmd --permanent --zone=public --add-port=514/udp
 sudo firewall-cmd --reload
 #sudo sleep 60s
 #sudo rm /var/lib/waagent/Microsoft.Azure.Diagnostics.LinuxDiagnostic-4.1.6/DistroSpecific.py
-wget -O diagfix.sh https://raw.githubusercontent.com/schoonercg/azuretools/master/diagfix.sh
-sudo diagfix.sh &
+#wget -O diagfix.sh https://raw.githubusercontent.com/schoonercg/azuretools/master/diagfix.sh
+#sudo diagfix.sh &
 #sudo sleep 60 && wget https://raw.githubusercontent.com/Azure/azure-linux-extensions/master/Diagnostic/DistroSpecific.py /var/lib/waagent/Microsoft.Azure.Diagnostics.LinuxDiagnostic-4.1.6/DistroSpecific.py -q
